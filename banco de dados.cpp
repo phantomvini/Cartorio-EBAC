4d8b09fbcab5c844e0aaf2da5e323c16c8df2151
phantomvini <143561910+phantomvini@users.noreply.github.com>
2023-08-29 20:12:12 -0300

    Add files via upload

--- banco de dados.cpp
+++ banco de dados.cpp
@@ -129,6 +129,7 @@ int main ()
 	printf ("\t1 - Registrar nomes\n");
 	printf ("\t2 - Consultar nomes\n");
 	printf ("\t3 - Deletar nomes\n\n");
+	printf ("\t4 - Sair do sistema\n\n");
 	printf ("Este software é de livre propriedade dos alunos\n");
 	printf ("Opção:");
 	scanf ("%d", &opcao);//Armazenando escolha
@@ -148,6 +149,11 @@ int main ()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf ("Muito obrigado por utilizar o sistema!");
+		return 0;
 		break;
 		
 		default:
